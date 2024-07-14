3fa5ad723dc9d1e6f61f504ddf4f6a816dbbd26b
ces08 <christinesong08@gmail.com>
2024-07-13 23:40:02 -0400

    watchlist tags added

--- js/watchlist.js
+++ js/watchlist.js
@@ -3,13 +3,65 @@ const progList= document.getElementById("inprog-list");
 const addBulletWatch = document.getElementById("add-bullet-watch");
 const addBulletProg = document.getElementById("add-bullet-prog");
 
+var tagList = [];
+const tagInput = document.getElementById("add-tags");
+
 addBulletWatch.addEventListener("keyup", function(event){
     if (event.key === "Enter"){
         console.log(addBulletWatch.value);
-        watchList.innerHTML += `
-        <li onclick = "removeWatchShow(this)">${addBulletWatch.value}</li>`
+        const watchTitle = `<li onclick = "removeWatchShow(this)"><div class = 'title'>${addBulletWatch.value}</div>`
+        let tagArr = `<div class = 'tags'>\n`;
+        tagList.forEach(e=>{
+            tagArr += `<span class = ${e.toLowerCase()}>${e}</span>\n`;
+            console.log(e);
+        });
+        watchList.innerHTML += (watchTitle +tagArr + `</div></li>`);
     }
 })
+
+const genres = ['sports','action','shonen','fantasy','romance','isekai','mystery','shoujo','sci-fi','comedy','slice-of-life'];
+
+function addTag(){
+    let newTag = tagList.at(-1);
+    if (genres.includes(newTag.toLowerCase())){
+        let tagHTML = `<span class = ${newTag.toLowerCase()}>${newTag}<i class="fa-solid fa-xmark" onclick = 'remove(this, "${newTag}")'></i></span>`;
+        tagInput.insertAdjacentHTML('beforeBegin', tagHTML);
+    } 
+    if (tagList.length >=2){
+        tagInput.classList.toggle('hidden');
+    }
+}
+function remove(obj, tagToDel){
+    let i = tagList.indexOf(tagToDel);
+    console.log(i);
+    tagList.splice(i, 1);
+    obj.parentElement.remove();
+    console.log(tagList);
+    if (tagList.length ==1){
+        tagInput.classList.toggle('hidden');
+    }
+}
+
+
+tagInput.addEventListener("keyup", function(event){
+    if (tagList.length < 2){
+        if (event.key === "Enter"){
+            let newTag = event.target.value.replace(/\s+/g, ''); //remove white space
+            if (genres.includes(newTag.toLowerCase())){
+            newTag = newTag.charAt(0).toUpperCase() + newTag.slice(1).toLowerCase();
+                if (newTag.length>1 && !tagList.includes(newTag)){
+                        tagList.push(newTag);
+                        addTag();
+                }
+                console.log(tagList);
+                event.target.value = "";
+            }
+        }
+    }
+})
+
+
+
 addBulletProg.addEventListener("keyup", function(event){
     if (event.key === "Enter"){
         console.log(addBulletProg.value);
