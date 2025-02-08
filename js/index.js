bdebb6f552d6c97936f162f74e43a9bfc0c4de41
ces08 <christinesong08@gmail.com>
2025-02-08 16:38:39 -0500

    scrollDown function index.html

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-// const box = document.getElementById("right-box");
-// box.addEventListener("mousemove", function(e){
-//     document.querySelectorAll('.data-widget').forEach(function(move){
-//         var moving_value = move.getAttribute("data-value");
-//         var x = e.clientX * moving_value/300;
-//         var y = e.clientY * moving_value/300;
-//         move.style.transform = 'translateX('+x+'px) translateY('+y+'px)';
-//     })
-// })
+function scrollDown(){
+    s2.scrollIntoView({behavior: "smooth",block: 'nearest'});
+}
